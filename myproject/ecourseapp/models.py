9cdb220ae1501808fce858e8ad00830be51eca7c
BoTran <botran612001@gmail.com>
2023-03-02 00:18:47 +0700

    model slice 26

--- myproject/ecourseapp/models.py
+++ myproject/ecourseapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    subject = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.subject
